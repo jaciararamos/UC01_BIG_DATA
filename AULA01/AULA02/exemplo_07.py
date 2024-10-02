530d8fc50bbaa52e52b6d75451a33fadecf6716d
jaciararamos <jaciararamos.jr@gmail.com>
2024-10-01 21:21:32 -0300

    Add files via upload

---
+++ AULA01/AULA02/exemplo_07.py
@@ -0,0 +1,4 @@
+h = float(input("Informe a altura: "))
+m = (72.7 * h) - 58
+f = (62.1 * h) - 44.7
+print(f"O peso ideal para homens é {m:.2f} e o peso ideal para as mulheres é {f:.2f}")
---
+++ AULA01/AULA02/exemplo_09.py
@@ -0,0 +1,9 @@
+import datetime
+data_atual = datetime.date.today()
+ano_atual = data_atual.year
+mes_atual = data_atual.month
+dia_atual = data_atual.day
+print(data_atual)
+print(ano_atual)
+print(mes_atual)
+print(dia_atual)
